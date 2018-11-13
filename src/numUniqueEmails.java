2d25c023bbb8599cb1685a877f82f3dad1a5702b
WindAsMe <542636539@qq.com>
2018-11-13 19:03:38 +0800

    Java For LeetCode

--- src/numUniqueEmails.java
+++ src/numUniqueEmails.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Author     : WindAsMe
  * File       : numUniqueEmails.java
@@ -8,6 +11,33 @@
 public class numUniqueEmails {
 
     private static int numUniqueEmailsResult(String[] emails) {
+        Set<String> set = new HashSet<>();
+        for (String email : emails)
+            set.add(process(email));
 
+        for (String s : set)
+            System.out.println(s);
+        return set.size();
+    }
+
+    private static String process(String email) {
+        StringBuilder s = new StringBuilder(email);
+        StringBuilder temp = new StringBuilder();
+        int i = 0;
+        for (; s.charAt(i) != '@'; i++) {
+            if (s.charAt(i) == '.') {
+                continue;
+            } else if (s.charAt(i) == '+') {
+                temp = new StringBuilder();
+            } else {
+                temp.append(s.charAt(i));
+            }
+        }
+        return temp.append(s.substring(i, s.length())).toString();
+    }
+
+    public static void main(String[] args) {
+        String[] emails = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+        System.out.println(numUniqueEmailsResult(emails));
     }
 }
