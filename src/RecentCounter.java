3ce3bf9b90943db0747f6aad4a43454fee878d13
WindAsMe <542636539@qq.com>
2018-11-19 20:34:55 +0800

    Java For LeetCode

---
+++ src/RecentCounter.java
@@ -0,0 +1,23 @@
+/**
+ * Author     : WindAsMe
+ * File       : RecentCounter.java
+ * Time       : Create on 18-11-19
+ * Location   : ../Home/JavaForLeetCode/RecentCounter.java
+ * Function   : LeetCode No.933
+ */
+public class RecentCounter {
+
+    public RecentCounter() {
+
+    }
+
+    public int ping(int t) {
+
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
