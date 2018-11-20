e4f303c4a92395bf0bca1fc84300a1034cc7d1d0
WindAsMe <542636539@qq.com>
2018-11-20 20:19:13 +0800

    Java For LeetCode

--- src/stoneGame.java
+++ src/stoneGame.java
@@ -10,9 +10,9 @@ public class stoneGame {
     private static boolean stoneGameResult(int[] piles) {
         int n = piles.length;
         int[][] dp = new int[n][n];
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++)
             dp[i][i] = piles[i];
-        }
+
         for(int dis = 1; dis < n; dis++) {
             for(int i = 0; i < n - dis; i++)
                 dp[i][i + dis] = Math.max(piles[i] - dp[i + 1][i + dis], piles[i + dis] - dp[i][i + dis - 1]);
