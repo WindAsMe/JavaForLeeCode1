0555d7528a66fc189a55a866924a5bb272109508
WindAsMe <542636539@qq.com>
2018-12-13 21:02:39 +0800

    Java For LeetCode

--- src/numSpecialEquivGroups.java
+++ src/numSpecialEquivGroups.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Author     : WindAsMe
  * File       : numSpecialEquivGroups.java
@@ -6,4 +9,26 @@
  * Function   : LeetCode No.893
  */
 public class numSpecialEquivGroups {
+
+    private static int numSpecialEquivGroupsResult(String[] A) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String a : A) {
+            StringBuilder even = new StringBuilder();
+            StringBuilder odd = new StringBuilder();
+            for (int i = 0; i < A.length; i += 2)
+                even.append(A[i]);
+            for (int i = 1; i < A.length; i += 2)
+                odd.append(A[i]);
+            if (map.containsKey(even.append(odd).toString()))
+                map.put(even.append(odd).toString(), map.get(even.append(odd).toString()) + 1);
+            else
+                map.put(even.append(odd).toString(), 1);
+        }
+        return map.size();
+    }
+
+    public static void main(String[] args) {
+        String[] A = {};
+        System.out.println();
+    }
 }
