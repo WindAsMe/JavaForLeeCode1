e3b833ce761a509eb98f648250844859dc9689da
WindAsMe <542636539@qq.com>
2018-12-13 20:39:18 +0800

    Java For LeetCode

---
+++ src/numSpecialEquivGroups.java
@@ -0,0 +1,9 @@
+/**
+ * Author     : WindAsMe
+ * File       : numSpecialEquivGroups.java
+ * Time       : Create on 18-12-13
+ * Location   : ../Home/JavaForLeetCode/numSpecialEquivGroups.java
+ * Function   : LeetCode No.893
+ */
+public class numSpecialEquivGroups {
+}
