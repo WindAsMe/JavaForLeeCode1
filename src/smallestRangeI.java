b034f8604269c76a045079a98e61360e3b085a74
WindAsMe <542636539@qq.com>
2018-12-21 20:07:27 +0800

    Java For LeetCode

---
+++ src/smallestRangeI.java
@@ -0,0 +1,22 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * Author     : WindAsMe
+ * File       : smallestRangeI.java
+ * Time       : Create on 18-12-21
+ * Location   : ../Home/JavaForLeetCode/smallestRangeI.java
+ * Function   : LeetCode No.908
+ */
+public class smallestRangeI {
+
+    private static int smallestRangeIResult(int[] A, int K) {
+        Arrays.sort(A);
+        return A[A.length - 1] - A[0] <= 2 * K ? 0 : A[A.length - 1] - A[0] - 2 * K;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1, 3, 6};
+
+    }
+}
