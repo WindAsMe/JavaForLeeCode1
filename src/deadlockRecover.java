049c7d5d13101f7a55127236a7856bdff79a67ab
WindAsMe <542636539@qq.com>
2018-12-21 20:51:53 +0800

    Java For LeetCode

---
+++ src/deadlockRecover.java
@@ -0,0 +1,17 @@
+/**
+ * Author     : WindAsMe
+ * File       : deadlockRecover.java
+ * Time       : Create on 18-12-21
+ * Location   : ../Home/JavaForLeetCode/deadlockRecover.java
+ * Function   : Recover the deadlock
+ */
+public class deadlockRecover {
+
+    private static boolean recover(int[] resource, int[][] current, int[][] needed) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
