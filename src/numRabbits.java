57e277c8aed360594a0a3875df9c1f7b92d51a54
WindAsMe <542636539@qq.com>
2018-12-24 20:33:49 +0800

    Java For LeetCode

--- src/numRabbits.java
+++ src/numRabbits.java
@@ -13,13 +13,13 @@ public class numRabbits {
     private static int numRabbitsResult(int[] answers) {
         int ans = 0;
         Map<Integer, Integer> map = new HashMap<>();
+
         for (int i : answers) {
             if (map.containsKey(i))
                 map.put(i, map.get(i) + 1);
             else
                 map.put(i, 1);
         }
-
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             int div = entry.getValue() / (entry.getKey() + 1);
             int per = entry.getValue() % (entry.getKey() + 1);
