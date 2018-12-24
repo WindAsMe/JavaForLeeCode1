210992fb31521829f617730103d54769f28d772b
WindAsMe <542636539@qq.com>
2018-12-24 20:28:20 +0800

    Java For LeetCode

---
+++ src/numRabbits.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author     : WindAsMe
+ * File       : numRabbits.java
+ * Time       : Create on 18-12-24
+ * Location   : ../Home/JavaForLeetCode/numRabbits.java
+ * Function   : LeetCode No.781
+ */
+public class numRabbits {
+
+    private static int numRabbitsResult(int[] answers) {
+        int ans = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : answers) {
+            if (map.containsKey(i))
+                map.put(i, map.get(i) + 1);
+            else
+                map.put(i, 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int div = (entry.getKey() + 1) / entry.getValue();
+            int per = (entry.getKey() + 1) % entry.getValue();
+            if (per > 0)
+                ans += entry.getKey() * (div + 1);
+            else
+                ans += entry.getKey() * div;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] ans = {};
+        System.out.println(numRabbitsResult(ans));
+    }
+}
