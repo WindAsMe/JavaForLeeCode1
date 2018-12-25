bb91d81c82d0eeb5f8b6290077412e0ba26c743d
WindAsMe <542636539@qq.com>
2018-12-25 17:28:00 +0800

    Java For LeetCode

--- src/deadlockRecover.java
+++ src/deadlockRecover.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Author     : WindAsMe
  * File       : deadlockRecover.java
@@ -7,11 +12,49 @@
  */
 public class deadlockRecover {
 
-    private static boolean recover(int[] resource, int[][] current, int[][] needed) {
+    private static boolean recover(int[] resource, int[][] current, int[][] needed, List<Integer> list) {
+        for (int i = 0; i < current.length; i++) {
+            if (list.size() == current.length)
+                return true;
+            else if (!list.contains(i)) {
+                if (judge(resource, needed[i])) {
+                    int[] r = resource.clone();
+                    int[][] c = current.clone();
+                    List<Integer> list1 = new ArrayList<>(list);
+                    list1.add(i);
+                    for (int j = 0; j < r.length; j++) {
+                        r[j] += c[i][j];
+                        c[i][j] = 0;
+                    }
+                    if (recover(r, c, needed, list1))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Current resource can satisfy the needed
+    private static boolean judge(int[] resource, int[] needed) {
+        for (int i = 0; i < resource.length; i++) {
+            if (resource[i] < needed[i])
+                return false;
+        }
         return true;
     }
 
     public static void main(String[] args) {
-
+        int[] resource = {2, 1, 0, 0};
+        int[][] current = {
+                {0, 0, 1, 0},
+                {2, 0, 0 ,1},
+                {0, 1, 2, 0}
+        };
+        int[][] needed = {
+                {2, 0, 0, 1},
+                {1, 0, 1, 0},
+                {2, 1, 0, 0}
+        };
+        System.out.println(recover(resource, current, needed, new ArrayList<>()));
     }
 }
