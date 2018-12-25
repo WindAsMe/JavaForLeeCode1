f27261bf3adeca39d057fc38c8a55b8f73aa5e0b
WindAsMe <542636539@qq.com>
2018-12-25 17:54:10 +0800

    Java For LeetCode

---
+++ src/customSortString.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author     : WindAsMe
+ * File       : customSortString.java
+ * Time       : Create on 18-12-25
+ * Location   : ../Home/JavaForLeetCode/customSortString.java
+ * Function   : LeetCode No.791
+ */
+public class customSortString {
+
+    private static String customSortStringResult(String S, String T) {
+        char[] s = S.toCharArray();
+        char[] t = T.toCharArray();
+        StringBuilder builder = new StringBuilder();
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : t) {
+            if (map.containsKey(c))
+                map.put(c, map.get(c) + 1);
+            else
+                map.put(c, 1);
+        }
+
+        for (char c : s) {
+            if (map.containsKey(c)) {
+                for (int i = 0; i < map.get(c); i++)
+                    builder.append(c);
+                map.remove(c);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            for (int i = 0; i < entry.getValue(); i++)
+                builder.append(entry.getKey());
+        }
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        String S = "cba";
+        String T = "abcd";
+        System.out.println(customSortStringResult(S, T));
+    }
+}
