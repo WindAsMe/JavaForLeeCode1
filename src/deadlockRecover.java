9898b9604c004925b0f59bac178d8ee83438ae37
WindAsMe <542636539@qq.com>
2018-12-25 17:31:10 +0800

    Java For LeetCode

--- src/deadlockRecover.java
+++ src/deadlockRecover.java
@@ -45,14 +45,14 @@ public class deadlockRecover {
     public static void main(String[] args) {
         int[] resource = {2, 1, 0, 0};
         int[][] current = {
-                {0, 0, 1, 0},
+                {0, 0, 0, 0},
                 {2, 0, 0 ,1},
                 {0, 1, 2, 0}
         };
         int[][] needed = {
-                {2, 0, 0, 1},
+                {1, 1, 0, 0},
                 {1, 0, 1, 0},
-                {2, 1, 0, 0}
+                {2, 2, 0, 0}
         };
         System.out.println(recover(resource, current, needed, new ArrayList<>()));
     }
