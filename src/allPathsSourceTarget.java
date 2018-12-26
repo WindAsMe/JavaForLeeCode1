8d3e51a809e87c733e07735b57b3d748eafa6224
WindAsMe <542636539@qq.com>
2018-12-26 13:32:06 +0800

    Java For LeetCode

--- src/allPathsSourceTarget.java
+++ src/allPathsSourceTarget.java
@@ -1,9 +1,33 @@
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Author     : WindAsMe
  * File       : allPathsSourceTarget.java
  * Time       : Create on 18-12-26
  * Location   : ../Home/JavaForLeetCode/allPathsSourceTarget.java
- * Function   : LeetCode No.
+ * Function   : LeetCode No.797
  */
 public class allPathsSourceTarget {
+
+    private static List<List<Integer>> allPathsSourceTargetResult(int[][] graph) {
+        int[][] adjacency = new int[graph.length][graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph[i].length; j++)
+                adjacency[i][graph[i][j]] = 1;
+        }
+        for (int[] g : graph)
+            System.out.println(Arrays.toString(g));
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = {
+                {1,2},
+                {3},
+                {3},
+                {}
+        };
+        System.out.println(allPathsSourceTargetResult(graph));
+    }
 }
