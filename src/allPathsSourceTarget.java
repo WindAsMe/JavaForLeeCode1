379cf0c3dd6cd659bb7d5a8d70e7f2ffb2c10a34
WindAsMe <542636539@qq.com>
2018-12-26 13:47:42 +0800

    Java For LeetCode

--- src/allPathsSourceTarget.java
+++ src/allPathsSourceTarget.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,13 +13,35 @@ public class allPathsSourceTarget {
 
     private static List<List<Integer>> allPathsSourceTargetResult(int[][] graph) {
         int[][] adjacency = new int[graph.length][graph.length];
+        List<List<Integer>> lists = new ArrayList<>();
         for (int i = 0; i < graph.length; i++) {
             for (int j = 0; j < graph[i].length; j++)
                 adjacency[i][graph[i][j]] = 1;
         }
-        for (int[] g : graph)
-            System.out.println(Arrays.toString(g));
-        return null;
+        for (int i = 1; i < graph.length; i++) {
+            if (adjacency[0][i] == 1) {
+                List<Integer> list = new ArrayList<>();
+                list.add(0);
+                recursion(adjacency, lists, list, i);
+            }
+        }
+        return lists;
+    }
+
+    private static void recursion(int[][] adjacency, List<List<Integer>> lists, List<Integer> list, int last) {
+        int end = adjacency.length - 1;
+        if (last == end) {
+            list.add(last);
+            lists.add(list);
+        } else if (list.size() < end) {
+            list.add(last);
+            for (int i = 1; i < adjacency.length; i++) {
+                if (adjacency[last][i] == 1) {
+                    List<Integer> l = new ArrayList<>(list);
+                    recursion(adjacency, lists, l, i);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
