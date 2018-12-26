f38dfe44eda0089a1a78d7d47b516b47136e4167
WindAsMe <542636539@qq.com>
2018-12-26 13:22:44 +0800

    Java For LeetCode

---
+++ src/allPathsSourceTarget.java
@@ -0,0 +1,9 @@
+/**
+ * Author     : WindAsMe
+ * File       : allPathsSourceTarget.java
+ * Time       : Create on 18-12-26
+ * Location   : ../Home/JavaForLeetCode/allPathsSourceTarget.java
+ * Function   : LeetCode No.
+ */
+public class allPathsSourceTarget {
+}
