e20a923ebfba41b1e74428243b87aa506a9f3eb3
WindAsMe <542636539@qq.com>
2018-12-27 16:03:57 +0800

    Java For LeetCode

---
+++ src/fourSumCount.java
@@ -0,0 +1,13 @@
+/**
+ * Author     : WindAsMe
+ * File       : fourSumCount.java
+ * Time       : Create on 18-12-27
+ * Location   : ../Home/JavaForLeetCode/fourSumCount.java
+ * Function   : LeetCode No.454
+ */
+public class fourSumCount {
+
+    private static int fourSumCountResult(int[] A, int[] B, int[] C, int[] D) {
+
+    }
+}
