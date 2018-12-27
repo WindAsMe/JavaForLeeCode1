eba840c5ab94a296ffc72c9fbce074aa31480ebc
WindAsMe <542636539@qq.com>
2018-12-27 16:13:09 +0800

    Java For LeetCode

--- src/fourSumCount.java
+++ src/fourSumCount.java
@@ -8,6 +8,28 @@
 public class fourSumCount {
 
     private static int fourSumCountResult(int[] A, int[] B, int[] C, int[] D) {
+        // 用于记录AB两个数组的和, Key为A中元素和B中元素和, Value为这个和出现的次数
+        Map<Integer, Integer> mapAB = new HashMap<>();
+        int length = A.length;
+        int sum;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                sum = A[i] + B[j];
+                mapAB.put(sum, mapAB.getOrDefault(sum, 0) + 1);
+            }
+        }
 
+        int result = 0;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                sum = C[i] + D[j];
+                if (mapAB.containsKey(-sum)){
+                    result += mapAB.get(-sum);
+                }
+            }
+        }
+
+        return result;
+    }
     }
 }
