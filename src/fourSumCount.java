3a19a5499973dda6d9af231abd4869c7b3c7e64d
WindAsMe <542636539@qq.com>
2018-12-28 17:02:03 +0800

    Java For LeetCode

--- src/fourSumCount.java
+++ src/fourSumCount.java
@@ -34,6 +34,10 @@ public class fourSumCount {
     }
 
     public static void main(String[] args) {
-
+        int[] A = { 1, 2};
+        int[] B = {-2,-1};
+        int[] C = {-1, 2};
+        int[] D = { 0, 2};
+        System.out.println(fourSumCountResult(A, B, C, D));
     }
 }
