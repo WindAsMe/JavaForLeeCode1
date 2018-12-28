f4c27c9fd1edd4d99e1632b96af6c79a296f10a3
WindAsMe <542636539@qq.com>
2018-12-28 17:10:41 +0800

    Java For LeetCode

---
+++ src/construct.java
@@ -0,0 +1,9 @@
+/**
+ * Author     : WindAsMe
+ * File       : construct.java
+ * Time       : Create on 18-12-28
+ * Location   : ../Home/JavaForLeetCode/construct.java
+ * Function   : LeetCode No.427
+ */
+public class construct {
+}
