60d5821beb6c914bf5d59889828e5389528ee420
WindAsMe <542636539@qq.com>
2019-01-02 17:03:27 +0800

    Java For LeetCode

--- src/magicalString.java
+++ src/magicalString.java
@@ -8,5 +8,41 @@
 public class magicalString {
 
     private static int magicalStringResult(int n) {
+        if(n <= 0)
+            return 0;
+        if(n <= 3)
+            return 1;
+        StringBuilder base = new StringBuilder("122");
+        StringBuilder sub = new StringBuilder("12");
+        int counts = 1;
+        int index = 2;
+        int temp = 1;
+        while (base.length() < n) {
+            index = sub.length();
+            temp = base.charAt(index)-'0';
+            sub.append(temp);
+            if(temp==2) {
+                temp = base.charAt(base.length()-1)-'0';
+                if(temp == 2){
+                    base.append("11");
+                    if(base.length() <= n)
+                        counts += 2;
+                    else
+                        counts += 1;
+                }else {
+                    base.append("22");
+                }
+            }else {
+                temp = base.charAt(base.length()-1)-'0';
+                if(temp == 2){
+                    base.append("1");
+                    counts += 1;
+                }else {
+                    base.append("2");
+                }
+            }
+        }
+        return counts;
+
     }
 }
