96611f5168721bb30233fcdf7204255a3a6c6a62
WindAsMe <542636539@qq.com>
2019-01-02 16:50:48 +0800

    Java For LeetCode

---
+++ src/magicalString.java
@@ -0,0 +1,12 @@
+/**
+ * Author     : WindAsMe
+ * File       : magicalString.java
+ * Time       : Create on 19-1-2
+ * Location   : ../Home/JavaForLeetCode/magicalString.java
+ * Function   : LeetCode No.481
+ */
+public class magicalString {
+
+    private static int magicalStringResult(int n) {
+    }
+}
