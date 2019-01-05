de9883d8aed2c46ef27c732c10d430f9c134ab65
WindAsMe <542636539@qq.com>
2019-01-05 15:57:06 +0800

    Java For LeetCode

--- src/integerBreak.java
+++ src/integerBreak.java
@@ -8,18 +8,23 @@
 public class integerBreak {
 
     private static int integerBreakResult(int n) {
-        if(n == 2)    return 1;
-        if(n == 3)    return 2;
+        if(n == 2)
+            return 1;
+        if(n == 3)
+            return 2;
         // Divide
         int k1 = n % 3, k2 = n / 3;
-        if(k1 == 1)   k2 --;
+        if(k1 == 1)
+            k2 --;
         int sum = 1;
-        for(int i = 0 ; i < k2 ; i ++) {
+        for(int i = 0 ; i < k2 ; i ++)
             sum *= 3;
-        }
-        if(k1 == 1)   return sum * 4;
-        else if(k1 == 2)    return sum * 2;
-        else return sum;
+        if(k1 == 1)
+            return sum * 4;
+        else if(k1 == 2)
+            return sum * 2;
+        else
+            return sum;
     }
 
     public static void main(String[] args) {
