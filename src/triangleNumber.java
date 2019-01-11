336fb9e6cbb4973854e28f25d4275c28191ce9c0
WindAsMe <542636539@qq.com>
2019-01-11 18:09:15 +0800

    Java For LeetCode

--- src/triangleNumber.java
+++ src/triangleNumber.java
@@ -64,9 +64,8 @@ public class triangleNumber {
         for (int i = 0; i < nums.length; i ++) {
             for (int j = i + 1; j < nums.length; j ++) {
                 for (int k = j + 1; k < nums.length; k ++) {
-                    if (nums[i] + nums[j] > nums[k] && nums[i] + nums[k] > nums[j] && nums[j] + nums[k] > nums[i]) {
+                    if (nums[i] + nums[j] > nums[k] && nums[i] + nums[k] > nums[j] && nums[j] + nums[k] > nums[i])
                         count ++;
-                    }
                 }
             }
         }
