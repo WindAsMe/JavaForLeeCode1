bb9da61c2b06944dd1a048352cc9b22bc3c6a196
WindAsMe <542636539@qq.com>
2019-01-12 13:27:28 +0800

    Java For LeetCode

--- src/canPlaceFlowers.java
+++ src/canPlaceFlowers.java
@@ -9,14 +9,13 @@
 public class canPlaceFlowers {
 
     private static boolean canPlaceFlowersResult(int[] flowerbed, int n) {
-        if (flowerbed.length ==0) {
+        if (flowerbed.length ==0)
             return false;
-        } else if (flowerbed.length == 1) {
-            if (flowerbed[0] == 1) {
+        else if (flowerbed.length == 1) {
+            if (flowerbed[0] == 1)
                 return n == 0;
-            } else {
+            else
                 return n <= 1;
-            }
         }
         for (int i = 0; i < flowerbed.length; i ++) {
             if (i == 0) {
