07d49b3b20a9f62826b524749ad20e96ff118ff5
WindAsMe <542636539@qq.com>
2019-01-16 14:49:38 +0800

    Java For LeetCode

--- src/numIslands.java
+++ src/numIslands.java
@@ -8,9 +8,9 @@
 public class numIslands {
 
     private static int numIslandsResult(char[][] grid) {
-        if (grid.length == 0 || grid[0].length == 0) {
+        if (grid.length == 0 || grid[0].length == 0)
             return 0;
-        } else {
+        else {
             int count = 0;
             for (int i = 0 ; i < grid.length; i ++ ) {
                 for (int j = 0 ; j < grid[0].length ; j ++ ) {
