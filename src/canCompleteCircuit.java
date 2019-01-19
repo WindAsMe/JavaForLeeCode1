ff10231bd947a9e66f97ae8c9217b8b2412f1c0d
WindAsMe <542636539@qq.com>
2019-01-19 12:17:34 +0800

    Java For LeetCode

--- src/canCompleteCircuit.java
+++ src/canCompleteCircuit.java
@@ -13,7 +13,7 @@ public class canCompleteCircuit {
         int total = 0;
         int cur = 0;
         int res = 0;
-        for (int i = 0; i < gas.length; i++){
+        for (int i = 0; i < gas.length; i++) {
             total += gas[i]- cost[i];
             cur += gas[i] - cost[i];
             if (cur < 0){
