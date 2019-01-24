8fd20d49f5a0ed71bc007615910f5595ed46026d
WindAsMe <542636539@qq.com>
2019-01-24 18:14:59 +0800

    Java For LeetCode

--- src/combinationSum4.java
+++ src/combinationSum4.java
@@ -55,6 +55,6 @@ public class combinationSum4 {
     public static void main(String[] args) {
         int[] nums = {1,2,3};
         int target = 4;
-        System.out.println(combinationSum4Result(nums,target));
+        System.out.println(combinationSum4Result(nums, target));
     }
 }
