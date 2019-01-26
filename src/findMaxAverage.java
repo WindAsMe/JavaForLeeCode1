bfddd1fa51a9714d2bf7b8bb8da131fabe3f36d1
WindAsMe <542636539@qq.com>
2019-01-26 18:19:30 +0800

    Java For LeetCode

--- src/findMaxAverage.java
+++ src/findMaxAverage.java
@@ -37,7 +37,8 @@ public class findMaxAverage {
     }
 
     public static void main(String[] args) {
-        int nums[] = {1,12,-5,-6,50,3};
-        System.out.println(findMaxAverageResult1(nums, 4));
+        int nums[] = {1, 12, -5, -6, 50, 3};
+        int k = 4;
+        System.out.println(findMaxAverageResult1(nums, k));
     }
 }
