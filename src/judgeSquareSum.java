5102385b47374521a649a2b70a452393630370f8
WindAsMe <542636539@qq.com>
2019-01-28 21:26:26 +0800

    Java For LeetCode

--- src/judgeSquareSum.java
+++ src/judgeSquareSum.java
@@ -20,6 +20,7 @@ public class judgeSquareSum {
     }
 
     public static void main(String[] args) {
-        System.out.println(judgeSquareSumResult(0));
+        int c = 0;
+        System.out.println(judgeSquareSumResult(c));
     }
 }
