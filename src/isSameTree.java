41c6af238a1aff60b0cf430cb5835f56c84ad2ce
WindAsMe <542636539@qq.com>
2019-01-28 21:17:12 +0800

    Java For LeetCode

--- src/isSameTree.java
+++ src/isSameTree.java
@@ -29,7 +29,7 @@ public class isSameTree {
         if (left.size() != right.size())
             return false;
         else {
-            for (int i = 0 ; i < left.size() ; i++ ){
+            for (int i = 0; i < left.size(); i++) {
                 if (!left.get(i).equals(right.get(i)))
                     return false;
             }
