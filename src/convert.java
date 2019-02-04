cee85ebd70bed2cc5be0a8ada358de4cdd3c019d
WindAsMe <542636539@qq.com>
2019-02-04 17:57:05 +0800

    Java For LeetCode

--- src/convert.java
+++ src/convert.java
@@ -15,7 +15,7 @@ public class convert {
         for (int i = 0; i < numRows; i++) {
             for (int j = i; j < n; j += sep) {
                 ans.append(s.charAt(j));
-                if ( i != 0 && i != numRows - 1 && sep + j - 2 * i < n )
+                if (i != 0 && i != numRows - 1 && sep + j - 2 * i < n)
                     ans.append(s.charAt(sep + j - 2 * i));
             }
         }
