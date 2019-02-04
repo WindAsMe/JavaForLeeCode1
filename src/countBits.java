e02b631fd89a8740c10affbe559c937f82732fd0
WindAsMe <542636539@qq.com>
2019-02-04 17:58:37 +0800

    Java For LeetCode

--- src/countBits.java
+++ src/countBits.java
@@ -19,7 +19,7 @@ public class countBits {
     private static int helper(int n) {
         int count = 0;
         while(n != 0) {
-            n = n & (n-1);
+            n = n & (n - 1);
             count++;
         }
         return count;
