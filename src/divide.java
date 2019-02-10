d729e66809b07d166dc2b38dd00045ee711d7c5d
WindAsMe <542636539@qq.com>
2019-02-10 18:50:18 +0800

    Java For LeetCode

--- src/divide.java
+++ src/divide.java
@@ -37,6 +37,8 @@ public class divide {
     }
 
     public static void main(String[] args) {
-        System.out.println(divideResult(-2147483648, -1));
+        int dividend = -2147483648;
+        int divisor = -1;
+        System.out.println(divideResult(dividend, divisor));
     }
 }
