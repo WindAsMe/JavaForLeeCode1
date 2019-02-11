a2d342a5c1e95377871bef3a21d451f18e368c2e
WindAsMe <542636539@qq.com>
2019-02-11 19:45:11 +0800

    Java For LeetCode

--- src/flatten.java
+++ src/flatten.java
@@ -29,7 +29,7 @@ public class flatten {
         root.right = trans(root.left);
         root.left = null;
         TreeNode temp = root;
-        while(temp.right != null)
+        while (temp.right != null)
             temp = temp.right;
         temp.right = right;
         return root;
