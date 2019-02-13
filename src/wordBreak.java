e2cbbee2b9ae0789417c49f0f08e5f810b3a7f46
WindAsMe <542636539@qq.com>
2019-02-13 10:58:50 +0800

    Java For LeetCode

--- src/wordBreak.java
+++ src/wordBreak.java
@@ -16,7 +16,7 @@ public class wordBreak {
             return true;
         boolean[] dp = new boolean[s.length()];
         dp[0] = wordDict.contains(s.substring(0, 1));
-        for(int i = 1; i < s.length(); i ++) {
+        for(int i = 1; i < s.length(); i++) {
             // If index from 0 to i
             // word can be consisted
             if (wordDict.contains(s.substring(0, i + 1))) {
@@ -24,7 +24,7 @@ public class wordBreak {
             } else {
                 // From 0 to i
                 // If word can be consisted
-                for(int j = 0; j < i; j ++) {
+                for(int j = 0; j < i; j++) {
                     if (dp[j] && wordDict.contains(s.substring(j + 1, i + 1))) {
                         dp[i] = true;
                         break;
