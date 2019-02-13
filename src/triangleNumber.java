a954023f1b437e35d41fc529e327d5445420aa77
WindAsMe <542636539@qq.com>
2019-02-13 11:02:24 +0800

    Java For LeetCode

--- src/triangleNumber.java
+++ src/triangleNumber.java
@@ -37,8 +37,8 @@ public class triangleNumber {
             return 0;
         int count = 0;
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i ++) {
-            for (int j = i + 1; j < nums.length; j ++) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 int k = find(nums[i] + nums[j], j, nums);
                 if (k > j)
                     count += k - j - 1;
