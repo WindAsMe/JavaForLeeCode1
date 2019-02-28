6930e9eae8323e4448c4003ec88959404991f7f0
WindAsMe <542636539@qq.com>
2019-02-28 14:18:53 +0800

    Java For LeetCode

--- src/getMinimumDifference.java
+++ src/getMinimumDifference.java
@@ -43,9 +43,8 @@ public class getMinimumDifference {
             System.out.println(list.toString());
             int min = Integer.MAX_VALUE;
             for (int i = 0; i < list.size(); i++) {
-                for (int j = i + 1; j < list.size(); j++) {
+                for (int j = i + 1; j < list.size(); j++)
                     min = Math.min(min, Math.abs(list.get(j) - list.get(i)));
-                }
             }
             return min;
         }
