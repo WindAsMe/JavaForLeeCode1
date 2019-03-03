c7025f7c3e5cd901773b3b24c70cbf907e346eea
WindAsMe <542636539@qq.com>
2019-03-03 22:25:32 +0800

    Java For LeetCode

--- src/hammingDistance.java
+++ src/hammingDistance.java
@@ -14,6 +14,6 @@ public class hammingDistance {
     public static void main(String[] args) {
         int x = 1;
         int y = 3;
-        System.out.println(hammingDistanceResult(x, y3));
+        System.out.println(hammingDistanceResult(x, y));
     }
 }
