fdd2f576f89f4d64c0fa4f94b7c3e18c59759b6b
WindAsMe <542636539@qq.com>
2019-04-02 21:02:18 +0800

    Java For LeetCode

--- src/triangleNumber.java
+++ src/triangleNumber.java
@@ -15,7 +15,7 @@ public class triangleNumber {
         Arrays.sort(nums);
         for (int i = size - 1; i >= 2; i--) {
             int left = 0, right = i - 1;
-            while(left < right) {
+            while (left < right) {
                 if (nums[left] + nums[right] > nums[i]) {
                     count += (right - left);
                     right--;
