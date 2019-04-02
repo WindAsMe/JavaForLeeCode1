0da062bd2f561c345fe466bdf632c1ec4c15a754
WindAsMe <542636539@qq.com>
2019-04-02 21:00:56 +0800

    Java For LeetCode

--- src/numberOfArithmeticSlices.java
+++ src/numberOfArithmeticSlices.java
@@ -23,7 +23,7 @@ public class numberOfArithmeticSlices {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,7,9};
+        int[] nums = {1, 2, 3, 4, 5, 7, 9};
         System.out.println(numberOfArithmeticSlicesResult(nums));
     }
 }
