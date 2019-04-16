3b56a281bfe51320fc452681e44778140823baeb
WindAsMe <542636539@qq.com>
2019-04-16 22:29:44 +0800

    Java For LeetCode

--- src/shoppingOffers.java
+++ src/shoppingOffers.java
@@ -20,10 +20,34 @@ public class shoppingOffers {
                 List<Integer> newNeeds = new ArrayList<>();
                 for (int i = 0; i < needs.size(); i++)
                     newNeeds.add(needs.get(i) - list.get(i));
-                sum = Math.min(dfs(total, price, special, newNeeds), sum);
+                int a = dfs(total, price, special, newNeeds);
+                System.out.println("a: " + a);
+                sum = Math.min(a, sum);
             }
         }
         return sum;
+    }
+
+    private static int dfs(int total, List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        if (listZero(needs))
+            return total;
+        for (List<Integer> list : special) {
+            if (listJudge(list, needs)) {
+                total += list.get(list.size() - 1);
+                List<Integer> newNeeds = new ArrayList<>();
+                for (int i = 0; i < needs.size(); i++)
+                    newNeeds.add(needs.get(i) - list.get(i));
+                if (listZero(newNeeds))
+                    return total;
+                total = Math.min(dfs(total, price, special, newNeeds), total);
+            }
+        }
+        if (!listZero(needs)) {
+            System.out.println("????");
+            for (int i = 0; i < needs.size(); i++)
+                total += price.get(i) * needs.get(i);
+        }
+        return total;
     }
 
     // l1.size() - l2.size() == 1
@@ -43,45 +67,21 @@ public class shoppingOffers {
         return true;
     }
 
-    private static int dfs(int total, List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
-        if (listZero(needs))
-            return total;
-        for (List<Integer> list : special) {
-            if (listJudge(list, needs)) {
-                total += list.get(list.size() - 1);
-                List<Integer> newNeeds = new ArrayList<>();
-                for (int i = 0; i < needs.size(); i++)
-                    newNeeds.add(needs.get(i) - list.get(i));
-                total = Math.min(dfs(total, price, special, newNeeds), total);
-            }
-        }
-        if (!listZero(needs)) {
-            for (int i = 0; i < needs.size(); i++)
-                total += price.get(i) * needs.get(i);
-        }
-        return total;
-    }
-
     public static void main(String[] args) {
         List<Integer> price = new ArrayList<>();
-        price.add(2);
-        price.add(5);
+        price.add(9);
+        price.add(9);
 
         List<List<Integer>> specials = new ArrayList<>();
         List<Integer> special1 = new ArrayList<>();
-        special1.add(3);
-        special1.add(0);
-        special1.add(5);
+        special1.add(1);
+        special1.add(1);
+        special1.add(1);
         specials.add(special1);
-        List<Integer> special2 = new ArrayList<>();
-        special2.add(1);
-        special2.add(2);
-        special2.add(10);
-        specials.add(special2);
 
         List<Integer> needs = new ArrayList<>();
-        needs.add(3);
-        needs.add(2);
+        needs.add(6);
+        needs.add(6);
         System.out.println(shoppingOffersResult(price, specials, needs));
     }
 }
