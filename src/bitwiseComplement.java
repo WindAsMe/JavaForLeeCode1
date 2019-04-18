2c33108f6210e7759f20c3f3cabb265a3abedad3
WindAsMe <542636539@qq.com>
2019-04-18 10:19:35 +0800

    Java For LeetCode

---
+++ src/bitwiseComplement.java
@@ -0,0 +1,21 @@
+/**
+ * Author     : WindAsMe
+ * File       : bitwiseComplement.java
+ * Time       : Create on 19-4-18
+ * Location   : ../Home/JavaForLeetCode/bitwiseComplement.java
+ * Function   : LeetCode No.1009
+ */
+public class bitwiseComplement {
+
+    public int bitwiseComplementResult(int N) {
+        if (N == 0)
+            return 1;
+        int t = N ^ Integer.MAX_VALUE;
+        int sum = 0;
+        for (int i = 0, d = 1; i < 30 && N != 0; i++, N /= 2) {
+            sum += d*((t>>i)&1);
+            d *= 2;
+        }
+        return sum;
+    }
+}
