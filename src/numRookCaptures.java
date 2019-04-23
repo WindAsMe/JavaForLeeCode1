7ced1db4c8b6373a75eb35df1bf5b76821d3c220
WindAsMe <542636539@qq.com>
2019-04-23 20:09:02 +0800

    Java For LeetCode

---
+++ src/numRookCaptures.java
@@ -0,0 +1,61 @@
+/**
+ * Author     : WindAsMe
+ * File       : numRookCaptures.java
+ * Time       : Create on 19-4-23
+ * Location   : ../Home/JavaForLeetCode/numRookCaptures.java
+ * Function   : LeetCode No.999
+ */
+public class numRookCaptures {
+
+    private static int numRookCapturesResult(char[][] board) {
+        int sum = 0;
+        int row = 0;
+        int column = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] == 'R') {
+                    row = i;
+                    column = j;
+                    break;
+                }
+            }
+        }
+
+        for (int i = row; i < 8; i++) {
+            if (board[i][column] == 'B')
+                break;
+            else if (board[i][column] == 'P') {
+                sum++;
+                break;
+            }
+        }
+
+        for (int i = row; i >= 0; i--) {
+            if (board[i][column] == 'B')
+                break;
+            else if (board[i][column] == 'P') {
+                sum++;
+                break;
+            }
+        }
+
+        for (int i = column; i < 8; i++) {
+            if (board[row][i] == 'B')
+                break;
+            else if (board[row][i] == 'P') {
+                sum++;
+                break;
+            }
+        }
+
+        for (int i = column; i >= 0; i--) {
+            if (board[row][i] == 'B')
+                break;
+            else if (board[row][i] == 'P') {
+                sum++;
+                break;
+            }
+        }
+        return sum;
+    }
+}
