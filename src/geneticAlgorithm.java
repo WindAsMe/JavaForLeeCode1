a47274023e154dc661278883fae8b7cdda5e2291
WindAsMe <542636539@qq.com>
2019-05-19 22:09:05 +0800

    Java For LeetCode

--- src/geneticAlgorithm.java
+++ src/geneticAlgorithm.java
@@ -8,9 +8,9 @@ public class geneticAlgorithm {
 
     static int popSize  = 100; // The amount of population
     int chromosomeSize = 0; // The size of chromosome
-    double mutationRate = 0.001; // The rate of being mutated
     int maxGene = 100; // The max generation
-
+    double Pc = 0.1; // The possibility of mutation
+    double Pm = 0.001; // The possibility of mutation
 
     static class individual { // The class of individual
         public double objectiveValue; // The value of individual
@@ -27,6 +27,7 @@ public class geneticAlgorithm {
     abstract class abstractEvolution { // The class to process the evolution
         int generation; // the amount of generation
         double average; // the average
+        int worstIndex; // The worst index
         individual Population[]; // population
         individual Best; // The best individual
         individual Worst; // The worst individual
@@ -67,9 +68,45 @@ public class geneticAlgorithm {
             if (popSize >= 0) // update the generation
                 System.arraycopy(newPopulation, 0, Population, 0, popSize);
         }
-        void VaryPop() {}
-        void CrossPop() {}
-        void OptimizePop() {}
+        void VaryPop() { // mutation operator
+            String str1 = "0";
+            String str2 = "1";
+            for (int index = 0; index < popSize; index++) {
+                for (int bitIndex = 0; bitIndex < chromosomeSize; bitIndex++) {
+                    double r = Math.random() * Integer.MAX_VALUE % 1000;
+                    r /= 1000;
+                    if (r < Pm) {
+                        if (Population[index].chromosome.charAt(bitIndex) == str1.charAt(0))
+                            Population[index].chromosome = Population[index].chromosome.substring(0, bitIndex) + str2.charAt(0) + Population[index].chromosome.substring(bitIndex);
+                        else
+                            Population[index].chromosome = Population[index].chromosome.substring(0, bitIndex) + str1.charAt(0) + Population[index].chromosome.substring(bitIndex);
+                    }
+                }
+            }
+        }
+        void CrossPop() {
+            for (int index = 0; index < popSize; index++) {
+                int r = (int) ((Math.random() * Integer.MAX_VALUE) % popSize);
+                individual temp = Population[index];
+                Population[index] = Population[r];
+                Population[r] = temp;
+            }
+            for (int index = 0; index < popSize; index += 2) {
+                double temp = (Math.random() * Integer.MAX_VALUE) % 1000 / 1000.0;
+                if (temp < Pc) { // create the new chromosome
+                    int Location = (int) (Math.random() * Integer.MAX_VALUE) % chromosomeSize;
+                    String str1 = Population[index].chromosome.substring(0, Location);
+                    String str2 = Population[index].chromosome.substring(Location);
+                    String str3 = Population[index + 1].chromosome.substring(0, Location);
+                    String str4 = Population[index + 1].chromosome.substring(Location);
+                    Population[index].chromosome = str1 + str4;
+                    Population[index + 1].chromosome = str3 + str2;
+                }
+            }
+        }
+        void OptimizePop() {
+            Worst = HistoryBest;
+        }
         void Initialization() { // Initialization the population
             for (int index = 0; index < popSize; index++) { // create every individual's chromosome
                 StringBuilder builder = new StringBuilder();
@@ -82,7 +119,24 @@ public class geneticAlgorithm {
             Statistics();
 
         }
-        void Statistics() {}
+        void Statistics() {
+            Best = Population[0];
+            Worst = Population[0];
+            double sum = 0;
+            for (int index = 0; index < popSize; index++)
+            {
+                if (Best.fitValue < Population[index].fitValue)
+                    Best = Population[index];
+                if (Worst.fitValue > Population[index].fitValue) {
+                    Worst = Population[index];
+                    worstIndex = index;
+                }
+                sum += Population[index].fitValue;
+            }
+            if (HistoryBest.fitValue < Best.fitValue)
+                HistoryBest = Best;
+            average = sum / popSize;
+        }
         void EvaluatePop() { // f(x1, x2) = x1 ^ 2 + x2 ^ 2;
             for (int index = 0; index < popSize; index++) {
                 int value1 = Integer.parseInt(Population[index].chromosome.substring(0, 3), 2);
@@ -115,12 +169,13 @@ public class geneticAlgorithm {
              Statistics();
              generation++;
         }
-        void output() {}
+        void output() {
+            System.out.println("Generation: " + generation + " Average: " + average + " Best: " + Best.fitValue + " Chromosome: " + Best.chromosome);
+        }
 
         public Evolution() { // Initialization
             Initialization();
             generation = 0;
         }
-
     }
 }
