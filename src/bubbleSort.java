7a7420493fdf53530ec28647002c3a29a283aa96
WindAsMe <542636539@qq.com>
2019-05-22 22:46:22 +0800

    Java For LeetCode

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -20,8 +20,32 @@ public class bubbleSort {
         return a;
     }
 
+    private static int[] quickSort(int[] a) {
+        quick(a, 0, a.length - 1);
+        return a;
+    }
+
+    private static void quick(int[] a, int start, int end) {
+        if (start >= end)
+            return;
+        int pivotal = a[start];
+        int left = start;
+        int right = end;
+        while (left < right) {
+            while (left < right && a[right] >= pivotal)
+                right--;
+            a[left] = a[right];
+            while (left < right && a[left] <= pivotal)
+                left++;
+            a[right] = a[left];
+        }
+        a[left] = pivotal;
+        quick(a, start, left - 1);
+        quick(a, left + 1, end);
+    }
+
     public static void main(String[] args) {
         int[] a = {61, 12, 6, 7, 9, 4, 66, 22, 3, 5, 6};
-        System.out.println(Arrays.toString(bubbleSortResult(a)));
+        System.out.println(Arrays.toString(quickSort(a)));
     }
 }
---
+++ src/interviewProblem.java
@@ -0,0 +1,11 @@
+package PACKAGE_NAME;
+
+/**
+ * Author     : WindAsMe
+ * File       : interviewProblem.java
+ * Time       : Create on 19-5-21
+ * Location   : ../Home/JavaForLeetCode/interviewProblem.java
+ * Function   : LeetCode No.
+ */
+public class interviewProblem {
+}
