dc0e18758c99ceabd0005e0d5ffb6cb370e1c7c6
WindAsMe <542636539@qq.com>
2019-06-07 21:55:00 +0800

    Java For LeetCode

--- src/trie.java
+++ src/trie.java
@@ -32,6 +32,18 @@ public class trie {
         }
     }
 
+    // Interface
+    private static List<Integer> Filter(String s, TrieTree tree) {
+        List<Integer> list = new ArrayList<>();
+        List<TrieTree> trees = tree.node;
+        for (int i = 0; i < s.length(); i++) {
+            int index = TrieTreeIndex(s.charAt(i), trees);
+            if (index != -1 && TrieTreeMatch(i + 1, s, tree.node.get(index)))
+                list.add(i);
+        }
+        return list;
+    }
+
     private static TrieTree TrieTreeConstruct(String[] words) {
         TrieTree tree = new TrieTree();
         for (String word : words)
@@ -66,9 +78,6 @@ public class trie {
         return -1;
     }
 
-
-
-
     private static void dfs(TrieTree tree) {
         if (tree.c != null) {
             System.out.println(tree.toString());
@@ -79,10 +88,22 @@ public class trie {
         }
     }
 
+    private static boolean TrieTreeMatch(int j, String s, TrieTree tree) {
+        if (tree.isContained)
+            return true;
+        if (j >= s.length())
+            return false;
+        int index = TrieTreeIndex(s.charAt(j), tree.node);
+        if (tree.node == null || index == -1)
+            return false;
+        else
+            return TrieTreeMatch(j + 1, s, tree.node.get(index));
+    }
+
     public static void main(String[] args) {
         String[] words = {"abc", "abbc", "abbe"};
         String s = "absababbebabbc";
         TrieTree tree = TrieTreeConstruct(words);
-
+        System.out.println(Filter(s, tree).toString());
     }
 }
